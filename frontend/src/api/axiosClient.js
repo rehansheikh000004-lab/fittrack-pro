a994775f155c220df2c414f5c34ef445a5d25f87
rehansheikh000004-lab <rehansheikh000004@gmail.com>
2025-12-02 13:37:06 +0530

    Add withCredentials option to axios client

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: " https://fittrack-pro-s5z6.onrender.com", // <- your backend URL
+  baseURL: "https://fittrack-pro-s5z6.onrender.com", //  your render URL
+  withCredentials: false
 });
