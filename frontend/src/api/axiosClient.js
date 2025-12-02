447aa2d6fcbf1c67a3dbf5c24bc592627b6afd6f
rehansheikh000004-lab <rehansheikh000004@gmail.com>
2025-12-02 12:54:08 +0530

    Update axios base URL for API requests

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://fittrack-pro-backend.onrender.com", // <- your backend URL
+  baseURL: " https://fittrack-pro-s5z6.onrender.com", // <- your backend URL
 });
